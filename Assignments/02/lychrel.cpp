b830f0cc635fe959f81758d376e62d6c65cc0b06
Kyle Pallo <kyle@Kyles-MacBook-Pro.local>
2023-10-12 18:11:00 -0700

    Initial loop unroll

--- Assignments/02/lychrel.cpp
+++ Assignments/02/lychrel.cpp
@@ -15,7 +15,7 @@
 #include <iostream>
 #include <vector>
 #include <thread>
-#include <array> 
+#include <array>
 
 #include "LychrelData.h"
 
@@ -51,35 +51,37 @@ int main() {
     for (int id = 0; id < MaxThreads; id++) {
         std::thread t{[=,&maxIter,&data,&records]() {
             std::vector<Number> numbers;
+            
             if (data.getNext(chunkSize,numbers)) {
-                for (int i = 0; i < numbers.size(); i++) {
-                    size_t iter = 0;
-                    Number n = numbers[i];
+                const size_t secondaryChunkSize = int(numbers.size()/8);
+                int i = 0;
+                for (int j = 0; j < secondaryChunkSize; j++) {
+                    size_t iter1 = 0;
+                    size_t iter2 = 0;
+                    size_t iter3 = 0;
+                    size_t iter4 = 0;
+                    size_t iter5 = 0;
+                    size_t iter6 = 0;
+                    size_t iter7 = 0;
+                    size_t iter8 = 0;
+                    Number n1 = numbers[i];
+                    Number n2 = numbers[i+1];
+                    Number n3 = numbers[i+2];
+                    Number n4 = numbers[i+3];
+                    Number n5 = numbers[i+4];
+                    Number n6 = numbers[i+5];
+                    Number n7 = numbers[i+6];
+                    Number n8 = numbers[i+7];
 
-                    // The Lychrel loop - for any iteration, take the number, reverse
-                    //   its digits, and sum those values together.  If that sum
-                    //   is a palindrome, stop processing
-                    while (!n.is_palindrome() && ++iter < MaxIterations) {
-                        Number sum(n.size());   // Value used to store current sum of digits
-                        Number r = n.reverse(); // reverse the digits of the value
+                    while (!n1.is_palindrome() && ++iter1 < MaxIterations) {
+                        Number sum(n1.size());   // Value used to store current sum of digits
+                        Number r = n1.reverse(); // reverse the digits of the value
 
-                        // An iterator pointing to the first digit of the reversed
-                        //   value.  This iterator will be incremented to basically
-                        //   traverse the digits of the main number in reverse
-                        auto rd = n.begin();
+                        auto rd = n1.begin();
                         
                         bool carry = false;  // flag to indicate if we had a carry
 
-                        // Sum the digits using the "transform" algorithm.  This
-                        //   algorithm traverses a range of values (in our case,
-                        //   starting with the least-siginificant [i.e., right most]
-                        //   digit) of the original number, adding each digit to its
-                        //   matching digit (by position) in the reversed number.
-                        //
-                        // The result is stored in the sum variable, which is
-                        //   built up one digit at a time, respecting if a carry
-                        //   digit was necessary for any iteration.
-                        std::transform(n.rbegin(), n.rend(), sum.rbegin(),
+                        std::transform(n1.rbegin(), n1.rend(), sum.rbegin(),
                             [&](auto d) {
                                 auto v = d + *rd++ + carry;
                 
@@ -90,32 +92,233 @@ int main() {
                             }
                         );
 
-                        // If there's a final carry value, prepend that to the sum
                         if (carry) { sum.push_front(1); }
 
-                        // Transfer the sum making it the next number to be processed
-                        //   (i.e., reversed, summed, and checked if it's a
-                        //   palindrome)
-                        n = sum;
+                        n1 = sum;
                     }
+                    
+                    while (!n2.is_palindrome() && ++iter2 < MaxIterations) {
+                        Number sum(n2.size());   // Value used to store current sum of digits
+                        Number r = n2.reverse(); // reverse the digits of the value
 
-                    // Update our records.  First, determine if we have a new
-                    //   maximum number of iterations that isn't the control limit
-                    //   (MaxIterations) for a particular number.  If we're less
-                    //   tha the current maximum (maxIter) or we've exceeded the number
-                    //   of permissible iterations, ignore the current result and move
-                    //   onto the next number.
-                    if (iter < maxIter || iter == MaxIterations) { continue; }
+                        auto rd = n2.begin();
+                        
+                        bool carry = false;  // flag to indicate if we had a carry
 
-                    // Otherwise update our records, which possibly means discarding
-                    //   our current maximum and rebuilding our records list.
-                    Record record{numbers[i], n};
-                    if (iter > maxIter) {
-                        records.clear();
-                        maxIter = iter;
+                        std::transform(n2.rbegin(), n2.rend(), sum.rbegin(),
+                            [&](auto d) {
+                                auto v = d + *rd++ + carry;
+                
+                                carry = v > 9;
+                                if (carry) { v -= 10; }
+                
+                                return v;
+                            }
+                        );
+
+                        if (carry) { sum.push_front(1); }
+
+                        n2 = sum;
                     }
+                    
+                    while (!n3.is_palindrome() && ++iter3 < MaxIterations) {
+                        Number sum(n3.size());   // Value used to store current sum of digits
+                        Number r = n3.reverse(); // reverse the digits of the value
 
-                    records.push_back(record);
+                        auto rd = n3.begin();
+                        
+                        bool carry = false;  // flag to indicate if we had a carry
+
+                        std::transform(n3.rbegin(), n3.rend(), sum.rbegin(),
+                            [&](auto d) {
+                                auto v = d + *rd++ + carry;
+                
+                                carry = v > 9;
+                                if (carry) { v -= 10; }
+                
+                                return v;
+                            }
+                        );
+
+                        if (carry) { sum.push_front(1); }
+
+                        n3 = sum;
+                    }
+                    
+                    while (!n4.is_palindrome() && ++iter4 < MaxIterations) {
+                        Number sum(n4.size());   // Value used to store current sum of digits
+                        Number r = n4.reverse(); // reverse the digits of the value
+
+                        auto rd = n4.begin();
+                        
+                        bool carry = false;  // flag to indicate if we had a carry
+
+                        std::transform(n4.rbegin(), n4.rend(), sum.rbegin(),
+                            [&](auto d) {
+                                auto v = d + *rd++ + carry;
+                
+                                carry = v > 9;
+                                if (carry) { v -= 10; }
+                
+                                return v;
+                            }
+                        );
+
+                        if (carry) { sum.push_front(1); }
+
+                        n4 = sum;
+                    }
+                    
+                    while (!n5.is_palindrome() && ++iter5 < MaxIterations) {
+                        Number sum(n5.size());   // Value used to store current sum of digits
+                        Number r = n5.reverse(); // reverse the digits of the value
+
+                        auto rd = n5.begin();
+                        
+                        bool carry = false;  // flag to indicate if we had a carry
+
+                        std::transform(n5.rbegin(), n5.rend(), sum.rbegin(),
+                            [&](auto d) {
+                                auto v = d + *rd++ + carry;
+                
+                                carry = v > 9;
+                                if (carry) { v -= 10; }
+                
+                                return v;
+                            }
+                        );
+
+                        if (carry) { sum.push_front(1); }
+
+                        n5 = sum;
+                    }
+                    
+                    while (!n6.is_palindrome() && ++iter6 < MaxIterations) {
+                        Number sum(n6.size());   // Value used to store current sum of digits
+                        Number r = n6.reverse(); // reverse the digits of the value
+
+                        auto rd = n6.begin();
+                        
+                        bool carry = false;  // flag to indicate if we had a carry
+
+                        std::transform(n6.rbegin(), n6.rend(), sum.rbegin(),
+                            [&](auto d) {
+                                auto v = d + *rd++ + carry;
+                
+                                carry = v > 9;
+                                if (carry) { v -= 10; }
+                
+                                return v;
+                            }
+                        );
+
+                        if (carry) { sum.push_front(1); }
+
+                        n6 = sum;
+                    }
+                    
+                    while (!n7.is_palindrome() && ++iter7 < MaxIterations) {
+                        Number sum(n7.size());   // Value used to store current sum of digits
+                        Number r = n7.reverse(); // reverse the digits of the value
+
+                        auto rd = n7.begin();
+                        
+                        bool carry = false;  // flag to indicate if we had a carry
+
+                        std::transform(n7.rbegin(), n7.rend(), sum.rbegin(),
+                            [&](auto d) {
+                                auto v = d + *rd++ + carry;
+                
+                                carry = v > 9;
+                                if (carry) { v -= 10; }
+                
+                                return v;
+                            }
+                        );
+
+                        if (carry) { sum.push_front(1); }
+
+                        n7 = sum;
+                    }
+                    
+                    if (iter1 >= maxIter && iter1 < MaxIterations) {
+                        Record record{numbers[i], n1};
+                        
+                        if (iter1 > maxIter) {
+                            records.clear();
+                            maxIter = iter1;
+                        }
+
+                        records.push_back(record);
+                    }
+                    
+                    if (iter2 >= maxIter && iter2 < MaxIterations) {
+                        Record record{numbers[i+1], n2};
+                        
+                        if (iter2 > maxIter) {
+                            records.clear();
+                            maxIter = iter2;
+                        }
+
+                        records.push_back(record);
+                    }
+                    
+                    if (iter3 >= maxIter && iter3 < MaxIterations) {
+                        Record record{numbers[i+2], n3};
+                        
+                        if (iter3 > maxIter) {
+                            records.clear();
+                            maxIter = iter3;
+                        }
+
+                        records.push_back(record);
+                    }
+                    
+                    if (iter4 >= maxIter && iter4 < MaxIterations) {
+                        Record record{numbers[i+3], n4};
+                        
+                        if (iter4 > maxIter) {
+                            records.clear();
+                            maxIter = iter4;
+                        }
+
+                        records.push_back(record);
+                    }
+                    
+                    if (iter5 >= maxIter && iter5 < MaxIterations) {
+                        Record record{numbers[i+4], n5};
+                        
+                        if (iter5 > maxIter) {
+                            records.clear();
+                            maxIter = iter5;
+                        }
+
+                        records.push_back(record);
+                    }
+                    
+                    if (iter6 >= maxIter && iter6 < MaxIterations) {
+                        Record record{numbers[i+5], n6};
+                        
+                        if (iter6 > maxIter) {
+                            records.clear();
+                            maxIter = iter6;
+                        }
+
+                        records.push_back(record);
+                    }
+                    
+                    if (iter7 >= maxIter && iter7 < MaxIterations) {
+                        Record record{numbers[i+6], n7};
+                        
+                        if (iter7 > maxIter) {
+                            records.clear();
+                            maxIter = iter7;
+                        }
+
+                        records.push_back(record);
+                    }
+                    
+                    i += 8;
                 }
             }
         }};
